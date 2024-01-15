46c0155b11a657ef3cf1dc7639edc89dbb5e88e6
Johnny98219 <johnnybrown98219>
2024-01-15 14:10:50 -0600

    #2: add search box

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,80 @@
             <i class="fas fa-tachometer-alt mr-1"></i>
             {{ __('Dashboard') }}
         </h2>
+
+        <div class="gb_od gb_yd gb_Se gb_We">
+            <div class="gb_De">
+                <form class="gb_Zd" method="get" role="search" id="aso_search_form_anchor"><button class="gb_Ke gb_Je"
+                        aria-label="Close search" type="button"><svg focusable="false" height="24px" viewBox="0 0 24 24"
+                            width="24px" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+                        </svg></button>
+                    <div class="gb_Pe gb_Je">
+                        <table cellspacing="0" cellpadding="0" class="gstl_50 gstt" role="presentation"
+                            style="height: 28px;">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <table cellspacing="0" cellpadding="0" style="width: 100%;">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="gsib_a gb_ve">
+                                                        <div id="gs_lc50" style="position: relative;"><input
+                                                                class="gb_ve" aria-label="Search bar" autocomplete="off"
+                                                                placeholder="Search" role="combobox" value="" name="q"
+                                                                type="text"
+                                                                style="border: none; padding: 0px; margin: 0px; height: auto; width: 100%; background: url(&quot;data:image/gif;base64,R0lGODlhAQABAID/AMDAwAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw%3D%3D&quot;) transparent; position: absolute; z-index: 6; left: 0px;"
+                                                                dir="ltr" spellcheck="false" aria-autocomplete="list"
+                                                                aria-haspopup="true" aria-live="off"
+                                                                aria-owns="gs_sbt50"><input disabled=""
+                                                                autocomplete="off" autocapitalize="off" id="gs_taif50"
+                                                                class="gb_ve" dir="ltr"
+                                                                style="border: none; padding: 0px; margin: 0px; height: auto; width: 100%; position: absolute; z-index: 1; background-color: transparent; -webkit-text-fill-color: silver; color: silver; left: 0px;">
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div><button class="gb_Le" aria-label="Clear search" type="button"><svg focusable="false"
+                            height="24px" viewBox="0 0 24 24" width="24px" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z">
+                            </path>
+                            <path d="M0 0h24v24H0z" fill="none"></path>
+                        </svg></button><button class="gb_Ie gb_Je" aria-label="Search" role="button"><svg
+                            focusable="false" height="24px" viewBox="0 0 24 24" width="24px"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M20.49,19l-5.73-5.73C15.53,12.2,16,10.91,16,9.5C16,5.91,13.09,3,9.5,3S3,5.91,3,9.5C3,13.09,5.91,16,9.5,16 c1.41,0,2.7-0.47,3.77-1.24L19,20.49L20.49,19z M5,9.5C5,7.01,7.01,5,9.5,5S14,7.01,14,9.5S11.99,14,9.5,14S5,11.99,5,9.5z">
+                            </path>
+                            <path d="M0,0h24v24H0V0z" fill="none"></path>
+                        </svg></button>
+                    <table cellspacing="0" cellpadding="0" class="gstl_50 gssb_c" role="presentation"
+                        style="width: 100%; position: relative; text-align: left; display: none;" dir="ltr">
+                        <tbody>
+                            <tr>
+                                <td class="gssb_n"></td>
+                                <td class="gssb_e" style="width: 100%;">
+                                    <table cellspacing="0" cellpadding="0" role="presentation" id="gs_sbt50"
+                                        class="gssb_m" style="width: 100%;">
+                                        <tbody role="listbox" class="ASO_SUGGESTIONS_CONTAINER"
+                                            aria-activedescendant="gs_ars50_0"></tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div
+                        style="background: transparent; color: rgb(0, 0, 0); padding: 0px; position: absolute; white-space: pre; visibility: hidden;">
+                    </div>
+                </form>
+            </div>
+        </div>
     </x-slot>
 
     {{-- Header --}}
@@ -40,7 +114,7 @@
                                 </div>
                             </div>
                         </div>
-                        
+
                     </div>
                 </div>
             </div>
